8e4f8fcb29b495b16994e0314c24f1be0a718ccb
David Vincelli <david@freshbooks.com>
2012-03-09 17:52:34 -0500

    made setup.py, removed src directory

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='settings',
+    version='0.1',
+    description='Declarative Schema for parsing INI files',
+    author='David Vincelli',
+    author_email='david@freshbooks.com',
+    url='https://github.com/ocim/settings',
+    py_modules = ['settings'],
+)
